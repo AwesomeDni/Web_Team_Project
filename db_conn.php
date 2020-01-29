2445ce1d6e81143473500fd6d8868e6243d4c14b
AwesomeDni <95eksldpf@naver.com>
2020-01-29 18:16:04 +0900

    2020.01.29 modified file. from daniel
    기존 DB연결을 PDO 객체를 사용하여 연결

--- db_conn.php
+++ db_conn.php
@@ -1,14 +1,25 @@
 <?php
 ## C:\xampp\php\pear 에 저장해야 reqire()로 불러올 수 있음
     //mysql 접속 계정 정보 설정
-    $servarname="localhost";
-    $username="root";
-    $password="1234";
-    $dbname="web_project";
-    //create connection 
-    $conn=new mysqli($servarname, $username, $password, $dbname);
-    //check connection 
-    if($conn->connect_error){
-        die("연결에 실패했습니다. 사유는 다음과 같습니다. :" ."<BR>" .$conn->connect_error);
+function DB_conn()
+{
+    $db_type="mysql";
+    $db_host="localhost";
+    $db_name="teampj";
+    $db_user="root";
+    $db_pass="password";
+
+    $dsn="$db_type:host=$db_host;dbname=$db_name;charset=utf8";
+
+    try
+    {   $pdo=new PDO($dsn,$db_user,$db_pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
     }
+    catch(PDOException $Exception)
+    {   die('error:'.$Exception->getMessage());    } 
+    
+    return $pdo; 
+}
+    
 ?>
