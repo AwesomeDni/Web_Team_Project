fd722b79f7bb31ac0bd28f5a09cbceeccf49d6ee
juhye963 <juhye9663@gmail.com>
2020-01-31 15:11:39 +0900

    비회원, 회원접속 구별. 로그인과 회원가입버튼 추가

--- main.php
+++ main.php
@@ -1,24 +1,34 @@
+<?php
+session_start();
+?>
 <html>
 <head>
 <title>home page</title>
 <meta http-equiv="content-type" content="txt/html" ; charset="utf-8">
 </head>
 <style>
-body {text-align: center; padding: 10px;}
+body {text-align: center;}
 </style>
 <body>
-<?php
-session_start();
-if(!isset($_SESSION['id'])){ //세션 존재하지 않는 경우, 즉 로그인상태 아닌 경우
-    header('Location:./login.html'); //로그인페이지로 바로 이동
-}
-?>
 <h2>Home Page<h2>
 <hr>
-<pre>
-홈 (로그인 성공)<br>
-여기는 홈페이지 입니다(게시글 페이지) <br><br>
+<?php
+//세션 존재하지 않는 경우, 즉 로그인상태 아닌 경우 비회원 접속
+if(!isset($_SESSION['id'])){
+?>
+<font size="4">비회원 접속</font>
+<p>로그인해주세요.</p>
+<a href="login.html"><button>로그인</button></a>
+<a href="join.html"><button>회원가입</button></a>
+<?php
+}else{
+?>
+<font size="5">회원접속</font>
+<p>환영합니다.</p>
 <a href=logout.php><button>로그아웃</button></a>
+<?php
+}
+?>
 <pre>
 </body>
 </html>
