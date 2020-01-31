de2240454f6ac581fcdfb61d5d95b22f0753a7a0
juhye963 <juhye9663@gmail.com>
2020-01-31 15:44:21 +0900

    pdo 수정

--- login.php
+++ login.php
@@ -1,23 +1,27 @@
 <?php
 session_start();
+
+require_once("db_conn.php");
+
+$pdo = DB_conn();
 $id=$_POST['id'];
 $pw=$_POST['pw'];
 
-$servername="localhost";
-$username="root";
-$password="1111";
-$dbname="project";
+try{
+    $sql="SELECT * FROM user_tb where id = :id";
+    $stmh = $pdo->prepare($sql);
+    $stmh->bindValue(':id',$id,PDO::PARAM_INT);
+    $stmh->execute();
+    $count=$stmh->rowCount();
+}catch(PDOException $Exception){
+   print 'error:'.$Exception->getMessage();
+} 
 
-$conn=mysqli_connect($servername,$username,$password,$dbname);
-
-$check="select * from user where id='$id'";
-
-$result=$conn->query($check);
-
-if($result->num_rows==1){
-    $row=$result->fetch_array(MYSQLI_ASSOC); 
-    //sql문 결과에서 하나의 열을 배열로 받아옴. 여기서는 연관배열로 받아오는 옵션줌 (MYSQLI_NUM 도 있음)
-    if($row['pw']==$pw && $row['id']==$id){ //컬럼이름을 key로 값 찾기 가능해짐
+if($count<0){
+    print "로그인 실패.";
+}else{
+    $row=$stmh->fetch(PDO::FETCH_ASSOC);
+    if($row['pw']==$pw && $row['id']==$id){
         $_SESSION['id']=$id;
         if(isset($_SESSION['id'])){
             header('location:./main.php');
@@ -27,6 +31,4 @@ if($result->num_rows==1){
     }else{
         print "패스워드나 아이디가 틀렸습니다.";
     }
-}else{
-    print "패스워드나 아이디가 틀렸습니다.";
 }
