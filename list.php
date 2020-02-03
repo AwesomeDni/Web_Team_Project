75b1a89bf71be65d82d5725876eeccde03cce87a
AwesomeDni <95eksldpf@naver.com>
2020-02-03 17:26:33 +0900

    2020.02.03 세션 사용, 로그인 체크 -daniel-
    세션을 사용하여 로그인 체크 후 로그인 or 로그아웃 버튼 출력
    로그인 상태이면 아이디 출력

--- list.php
+++ list.php
@@ -1,39 +1,63 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>Web Project</title>
 </head>
 <body>
-    <h1>게시글</h1>
-    <?php 
-    require_once('db_conn.php');
-    $pdo = DB_conn();
+<header>
     
-    try
-    {   //쿼리문 작성
-        $query = "select content_no,title,id from show_view order by content_no desc";
-        $stmh=$pdo->prepare($query); //sql문을 인잭션으로 부터 보호하기위한 처리
-        $stmh->execute();
-    }
-    catch(PDOException $e)
-    {   print 'err: '. $e->getMessage();   }
+<?php
+# 로그인 체크
+if(isset($_SESSION['id']))
+{   $id = $_SESSION['id'];
+    print $id.' 님';
     ?>
+	<button onclick="location.href='logout.php'"> 로그아웃</button>
+	<?php
+}
+else
+{?>
+	<button onclick="location.href='login.html'"> 로그인</button>
+	<?php
+}
+?>
+</header>
+<hr>
+<h1>게시글</h1>
 
-    <table border=1>
-        <tr align="center">
-            <td>글번호</td><td>제목</td><td>작성자</td>
-        </tr>
-    <?php
-    
-    while($row=$stmh->fetch(PDO::FETCH_ASSOC))
-    {   print "<tr>";
-        print "<td align=center>" . $row['content_no'] . "</td>";
-        print "<td width=500px><a href='show.php?id=". $row['content_no'] ."'>" . $row['title'] . "</a></td>";
-        print "<td align=center>".$row['id']."</td>";
-        print "</tr>";
-    }
-    ?>
-    </table>
-    <button onclick="location.href='insert.php'">글쓰기</button>
+<?php 
+# DB연결
+require_once('db_conn.php');
+$pdo = DB_conn();
+
+# 게시글 불러오기
+try
+{   //쿼리문 작성
+    $query = "select content_no,title,id,view_cnt from show_view order by content_no desc";
+    $stmh=$pdo->prepare($query); //sql문을 인잭션으로 부터 보호하기위한 처리
+    $stmh->execute();
+}
+catch(PDOException $e)
+{   print 'err: '. $e->getMessage();   }
+?>
+
+<table border=1>
+    <tr align="center">
+        <td>글번호</td><td>제목</td><td>작성자</td><td>조회수</td>
+    </tr>
+<?php
+# 게시글 리스트 출력
+while($row=$stmh->fetch(PDO::FETCH_ASSOC))
+{   print "<tr>";
+    print "<td align=center>" . $row['content_no'] . "</td>";
+    print "<td width=500px><a href='show.php?content_no=". $row['content_no'] ."'>" . $row['title'] . "</a></td>";
+    print "<td align=center>".$row['id']."</td>";
+    print "<td align=center>".$row['view_cnt']."</td>";
+    print "</tr>";
+}
+?>
+</table>
+<button onclick="location.href='insert.php'">글쓰기</button>
 </body>
 </html>
