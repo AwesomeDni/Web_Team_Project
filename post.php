8e58bde86f5b7a76646a0ce78c8f352ec09d753d
AwesomeDni <95eksldpf@naver.com>
2020-02-03 17:15:49 +0900

    2020.02.03 세션이용, 변수 이름 변경 -daniel-
    글 작성후 자신이 작성한 글 표시하는 페이지로 이동

--- post.php
+++ post.php
@@ -1,13 +1,29 @@
 <?php
 session_start();
+# insert.php로 부터 받은 정보 추출하여 변수에 저장
 $title = $_POST['title'];
-$content = ($_POST['content']);
-$date=date('Y-m-d H:i:s');
-?>
-<?php require('db_conn.php') ?>
-<?php
-//쿼리문 작성
+$content = $_POST['content'];
+
+$date=date('Y-m-d H:i:s');//글쓴 날짜를 위한 변수
+
+# DB 연결
+require('db_conn.php');
 $pdo = DB_conn();
+
+# 로그인한 id와 일치하는 user_no 추출 
+try
+{   $sql = "select user_no from user_tb where id= :id";
+    $stmh = $pdo->prepare($sql);
+    $stmh->bindValue(':id',$_SESSION['id']);
+    $stmh->execute();
+    while($row=$stmh->fetch(PDO::FETCH_ASSOC)){
+        $user_no = $row['user_no'];
+    }
+}
+catch(PDOException $e)
+{   print 'err:'.$e->getMessage();  }
+
+# DB에 form에서 입력한 정보 insert
 try
 {   //쿼리문 작성
     $query = "insert into contents_tb(title,content,user_no,category_no,write_dt) 
@@ -15,7 +31,7 @@ try
     $stmh=$pdo->prepare($query); //sql문을 인잭션으로 부터 보호하기 위한 처리
     $stmh->bindValue(':title',$title);
     $stmh->bindValue(':content',$content);
-    $stmh->bindValue(':user_no',$_SESSION['user_no']);
+    $stmh->bindValue(':user_no',$user_no);
     $stmh->bindValue(':category_no',$_SESSION['category_no']);
     $stmh->bindValue(':date',$date);
     $stmh->execute();
@@ -25,11 +41,12 @@ catch(PDOException $e)
     $pdo->rollBack();
 }
 
-try{
-    //쿼리문 작성
+# 방금 작성한 글 표시
+try
+{   //자신이 작성한 글 중 가장 최근에 쓴 글의 번호 추출
     $query = "select content_no from contents_tb where user_no= :no order by write_dt desc limit 1";
     $stmh=$pdo->prepare($query); //sql문을 인잭션으로 부터 보호하기 위한 처리
-    $stmh->bindValue(':no',$_SESSION['user_no'],PDO::PARAM_INT);
+    $stmh->bindValue(':no',$user_no,PDO::PARAM_INT);
     $stmh->execute();
     $row=$stmh->fetch(PDO::FETCH_ASSOC);
     $cno=$row['content_no'];
@@ -39,5 +56,5 @@ catch(PDOException $e){
     $pdo->rollBack();
 }
 
-print "<script>location.href='show.php?id=".$cno."';</script>";
+print "<script>location.href='show.php?content_no=".$cno."';</script>";
 ?>
