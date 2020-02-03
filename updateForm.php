e275b49deda952b63ed070718d8fef4fd54b7628
AwesomeDni <95eksldpf@naver.com>
2020-02-03 16:50:44 +0900

    2020.02.03 불필요 코드 삭제, 주석 수정 -daniel-

--- updateForm.php
+++ updateForm.php
@@ -2,11 +2,12 @@
 <html><head><title>php test</title></head>
 <body>
 <?php
-    require_once("db_conn.php"); //MYDB.php파일을 한번만 불러옴; 파일이 없으면 아래 코등 실행 x
+# DB연결 
+    require_once("db_conn.php"); //db_conn.php파일을 한번만 불러옴; 파일이 없으면 아래 코등 실행 x
     $pdo = DB_conn();//db에 연결된 pdo객체 생성하여 변수 $pdo에 할당
-    $no = 1; //아이디값을 1로 설정
-    $_SESSION['user_no']=$no;
+# 세션 설정
     $cno = $_SESSION['content_no'];
+# 문서번호에 해당하는 글 내용 가져오기
     try
     {   $sql = "SELECT * FROM contents_tb WHERE content_no= :cno"; 
         $stmh = $pdo->prepare($sql); //sql문을 인잭션으로 부터 보호하기위한 처리
@@ -16,7 +17,7 @@
     } 
     catch(PDOException $Exception)//에러 발생시 $Exception이라는 이름으로 PDO예외 처리 객체 생성
     {   print "error:".$Exception->getMessage();    }//객체에 내장된 오류메세지 정보를 얻어오는 함수를 실행후 출력
-
+# 수정 폼
     if($count<1)
     {   print "no have update data!<br>";   } //반환된 레코드가 없으면 출력
     else //있으면 실행
