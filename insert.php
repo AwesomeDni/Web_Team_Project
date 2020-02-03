b16e865c1a6534b8c311354bcdea201d8d7dc043
AwesomeDni <95eksldpf@naver.com>
2020-02-03 17:20:15 +0900

    2020.02.03 세션 사용 -daniel-
    세션을 이용하여 로그인 체크 후 로그인 or 로그아웃 버튼 출력
    로그인상태인 경우 아이디 표시
    비로그인 시 로그인 폼으로 이동

--- insert.php
+++ insert.php
@@ -4,18 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Web Project</title>
 </head>
 <body>
-<?php 
+<?php
+#세션 사용
 session_start();
-$_SESSION['user_no'] = 1;
-$_SESSION['category_no'] = 1;
+#로그인 체크
+if(isset($_SESSION['id']))
+{   $id = $_SESSION['id'];
+    print $id.' 님';
 ?>
-    <form method="POST" action="post">
+    <button onclick="location.href='logout.php'"> 로그아웃</button>
+<?php
+    $_SESSION['category_no'] = 1;
+
+# 입력 폼
+?>
+    <form method="POST" action="post.php" autocomplete="off">
         제목: <input type="text" name="title"><br>
         내용: <textarea name="content"></textarea>
         <input type="submit" value="확인">
     </form>
 </body>
 </html>
+<?php
+}
+# 비로그인 시 로그인 페이지로 이동
+else
+{   print "<script>alert('로그인 후에 작성 가능합니다.');</script>"; 
+    print "<script>location.href='login.html';</script>";
+}
+?>
