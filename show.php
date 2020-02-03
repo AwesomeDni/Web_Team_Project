0889898b5194ae0c4a99582bc69091aa6514496e
AwesomeDni <95eksldpf@naver.com>
2020-02-03 17:07:14 +0900

    2020.02.03 세션사용,로그인 체크, 코드 수정 -daniel-
    세션을 이용하여 로그인 체크 후 로그인 or 로그아웃 버튼 출력
    로그인 상태시 아이디 출력
    조회수 기능 추가
    조회수 리로딩으로 무한증가 방지로 쿠키 설정
    htmlspeecialchars코드 삭제
    content에 nl2br함수 적용
    작성자에게만 수정, 삭제버튼 출력

--- show.php
+++ show.php
@@ -1,41 +1,111 @@
+<title>Web Project</title>
 <?php session_start(); ?>
+<header>
 <?php
-$_SESSION['content_no'] = $content_no = $_GET['id'];
+#로그인 체크
+if(isset($_SESSION['id']))
+{   $id = $_SESSION['id'];
+    print $id.' 님';
+    ?>
+	<button onclick="location.href='logout.php'"> 로그아웃</button>
+<?php
+}
+else
+{   $id='';
+?>
+	<button onclick="location.href='login.html'"> 로그인</button>
+<?php
+}
+?>
+</header>
+<hr>
+
+<?php
+#DB연결
 require_once('db_conn.php');
 $pdo = DB_conn();
 
-try{
-    //쿼리문 작성
+#사용자가 요청한 문서 번호 획득
+$_SESSION['content_no'] = $content_no = $_GET['content_no'];
+
+#이전,다음 게시글 보기
+try
+{   //쿼리문 작성
+    $query = "select max(content_no) max, min(content_no) min from contents_tb";
+    $stmh=$pdo->prepare($query); //sql문을 인잭션으로 부터 보호하기 위한 처리
+    $stmh->execute();
+}
+catch(PDOException $e)
+{   print 'err: '. $e->getMessage();
+$pdo->rollBack();
+}
+##글번호의 최대,최소값을 구함
+while ($row=$stmh->fetch(PDO::FETCH_ASSOC)) {
+    $max=$row['max'];
+    $min=$row['min'];
+}
+##마지막 글에는 다음 페이지 표시 x
+if (!($max==($content_no))) {
+    print "<button onclick='location.href=\"show.php?content_no=".($content_no + 1)."\"'>다음 글 ▲</button>";
+}
+##처음 글에는 이전 페이지 표시 x 
+if (!($min==($content_no))) {
+    print "<button onclick='location.href=\"show.php?content_no=".($content_no - 1)."\"'>이전 글 ▼</button>";
+}
+
+#문서의 조회수
+$cnt_flag = 0;
+if(!isset($_COOKIE['view'.$content_no])) {//한 방문자가 새로고침으로 조회수를 올리는것 방지
+    $cnt_flag += 1;
+    setcookie('view'.$content_no,$cnt_flag);
+    try
+    {   //조회수 증가 쿼리
+        $query = "update contents_tb set view_cnt = view_cnt + 1 where content_no = :no";
+        $stmh=$pdo->prepare($query);
+        $stmh->bindValue(':no',$content_no);
+        $stmh->execute();
+    }
+    catch(PDOException $e)
+    {   print 'err: '. $e->getMessage();
+    $pdo->rollBack();
+    }
+}
+
+#DB에서 문서내용 가져오기
+try
+{   //쿼리문 작성
     $query = "select * from show_view where content_no = :no";
     $stmh=$pdo->prepare($query); //sql문을 인잭션으로 부터 보호하기 위한 처리
     $stmh->bindValue(':no',$content_no);
     $stmh->execute();
 }
-catch(PDOException $e){
-    print 'err: '. $e->getMessage();
+catch(PDOException $e)
+{   print 'err: '. $e->getMessage();
     $pdo->rollBack();
 }
 ?>
-<TABLE border="1" cellspacing="0" cellpadding="8">
+<TABLE border="1">
 <TBODY>
     <TR>
         <TH>No</TH>
         <TH width=100px>Title</TH>
         <TH width=300px>contetnt</TH>
-        <TH width=50px>작성자</TH>
+        <TH width=70px>작성자</TH>
         <TH>작성일</TH>
-        <TH width=50px>조회수</TH>
+        <TH width=70px>조회수</TH>
     </TR>
 <?php
+#문서 내용 출력
+$writer='';//작성자 저장하기 위한 변수
 while($row=$stmh->fetch(PDO::FETCH_ASSOC))//PDO::FETCH_ASSOC 결과값을 한 행씩 읽어오는 메소드
-{   ?>
+{?>
     <TR>
         <TD align="center"><?=htmlspecialchars($row['content_no'])?></TD>
-        <TD><?=htmlspecialchars($row['title'])?></TD>
-        <TD><?=htmlspecialchars($row['content'])?></TD>
-        <TD><?=htmlspecialchars($row['id'])?></TD>
-        <TD><?=htmlspecialchars($row['write_dt'])?></TD>
-        <TD><?=htmlspecialchars($row['view_cnt'])?></TD>
+        <TD><?=$row['title']?></TD>
+        <TD><?=nl2br($row['content'])?></TD>
+        <TD><?=$writer=$row['id']?></TD>
+        <TD><?=$row['write_dt']?></TD>
+        <TD><?=$row['view_cnt']?></TD>
     </TR>
 <?php 
 } 
@@ -43,6 +113,15 @@ while($row=$stmh->fetch(PDO::FETCH_ASSOC))//PDO::FETCH_ASSOC 결과값을 한 �
 </TBODY>
 </TABLE>
 
+<footer>
 <button onclick="location.href='list.php'">목록 보기</button>
-<button onclick="location.href='updateForm.php'">수정</button>
-<button onclick="location.href='delete.php'">삭제</button>
+<button onclick="location.href='insert.php'">글쓰기</button>
+<?php
+if($id==$writer)//글 작성자만 수정 및 삭제 가능
+{?>
+    <button onclick="location.href='updateForm.php'">수정</button>
+    <button onclick="location.href='delete.php'">삭제</button>
+<?php
+}
+?>
+</footer>
