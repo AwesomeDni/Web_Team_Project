1f9b1a39f2ed355d36dd22d15f8feff04e683e38
AwesomeDni <95eksldpf@naver.com>
2020-02-03 17:30:23 +0900

    2020.02.03 쿠키 삭제코드 추가 -daniel-

--- logout.php
+++ logout.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 $reset=session_destroy();//모든세션 지우기
+#모든 쿠키 지우기
+$past = time() - 3600;
+foreach ( $_COOKIE as $key => $value ) 
+{   setcookie( $key, $value, $past );  }
+
 if($reset){
     header('location: ./main.php'); 
     //세션지워서 로그아웃 성공하면 메인페이지로
