25e417b89c06c412171e6e3e6e56037eb237f9f9
juhye963 <juhye9663@gmail.com>
2020-02-04 10:34:44 +0900

    회원탈퇴(회원정보삭제)

--- accountDelete.php
+++ accountDelete.php
@@ -8,6 +8,11 @@ $id=$_POST['id'];
 $pw=$_POST['pw'];
 $email=$_POST['email'];
 
+#로그인상태확인
+if(!isset($_SESSION['id'])){
+    header('location: ./main.php');
+}
+
 #id확인(일치시 정보 갖고옴)
 if($id!=$_SESSION['id']){
     print "<script>alert('id does not match');</script>";
@@ -29,12 +34,28 @@ if(!$count){
 }else{
     while($row=$stmh->fetch(PDO::FETCH_ASSOC)){
         if($row['pw']!=$pw || $row['email']!=$email){
-            print "입력정보가 일치하지 않습니다."
+            print "입력정보가 일치하지 않습니다.";
         }else{
-            $pdo->beginTransaction();
-            $sql="DELETE FROM user_tb WHERE id=:id";
-            $stmh->bindValue(':id',$id,PDO::PARAM_STR);
+            try{
+                $pdo->beginTransaction();
+                $sql="DELETE FROM user_tb WHERE id=:id";
+                $stmh=$pdo->prepare($sql);
+                $stmh->bindValue(':id',$id,PDO::PARAM_STR);
+                $stmh->execute();
+                $pdo->commit();
+                $check=$stmh->rowCount();
+            }catch(PDOException $Exception){
+                $pdo->rollBack();
+                print 'error : '.$Exception->getMessage();
+            }
         }
     }
+}
+
+#회원탈퇴 성공하면 쿠키와 세션 지우고 메인으로
+if(!$check){
+    print "회원탈퇴에 실패하였습니다. 다시 시도해주세요.";
+}else{
+    header('location: ./main.php');
 }
 
