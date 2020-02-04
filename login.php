1eaaa30cee641bb5cb76cb791acbac3629014fa4
juhye963 <juhye9663@gmail.com>
2020-02-04 17:34:06 +0900

    로그인창 공백입력  오류 처리

--- login.php
+++ login.php
@@ -7,28 +7,37 @@ $pdo = DB_conn();
 $id=$_POST['id'];
 $pw=$_POST['pw'];
 
-try{
-    $sql="SELECT * FROM user_tb where id = :id";
-    $stmh = $pdo->prepare($sql);
-    $stmh->bindValue(':id',$id,PDO::PARAM_STR);
-    $stmh->execute();
-    $count=$stmh->rowCount();
-}catch(PDOException $Exception){
-   print 'error:'.$Exception->getMessage();
-} 
-
-if($count<0){
-    print "로그인 실패.";
-}else{
-    $row=$stmh->fetch(PDO::FETCH_ASSOC);
-    if($row['pw']==$pw && $row['id']==$id){
-        $_SESSION['id']=$id;
-        if(isset($_SESSION['id'])){
-            header('location:./main.php');
-        }else{
-            print "세션 저장 실패";
-        }
+#공백입력시 오류표시
+if ($id=="" || $pw == "")
+{
+    print "<script>alert('빈칸을 모두 채워주세요.');</script>";
+    print "<script>location.href='login.html';</script>";
+}
+else
+{
+    try{
+        $sql="SELECT * FROM user_tb where id = :id";
+        $stmh = $pdo->prepare($sql);
+        $stmh->bindValue(':id',$id,PDO::PARAM_STR);
+        $stmh->execute();
+        $count=$stmh->rowCount();
+    }catch(PDOException $Exception){
+       print 'error:'.$Exception->getMessage();
+    } 
+    
+    if($count<0){
+        print "로그인 실패.";
     }else{
-        print "패스워드나 아이디가 틀렸습니다.";
+        $row=$stmh->fetch(PDO::FETCH_ASSOC);
+        if($row['pw']==$pw && $row['id']==$id){
+            $_SESSION['id']=$id;
+            if(isset($_SESSION['id'])){
+                header('location:./main.php');
+            }else{
+                print "세션 저장 실패";
+            }
+        }else{
+            print "패스워드나 아이디가 틀렸습니다.";
+        }
     }
 }
