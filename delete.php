dd9addc359cdc45e72730b6a886e5ffbb9586d55
Daeil-Song <sdi9429@naver.com>
2020-02-05 15:33:08 +0900

    2020.02.05 게시글 삭제

---
+++ delete.php
@@ -0,0 +1,24 @@
+<?php session_start(); ?>
+<HTML>
+<HEAD>
+    <TITLE>PHP test</TITLE>
+</HEAD>
+<BODY>
+<?php
+require_once("db_conn.php");
+$pdo = DB_conn();
+$cno = $_SESSION['content_no'];
+# 문서번호에 해당하는 글 내용 가져오기
+    try
+    {   $sql = "DELETE FROM contents_tb WHERE content_no= :cno"; 
+        $stmh = $pdo->prepare($sql); //sql문을 인잭션으로 부터 보호하기위한 처리
+        $stmh->bindValue(':cno',$cno,PDO::PARAM_INT);
+        $stmh->execute();//sql문 실행
+        $count = $stmh->rowCount();//sql문 실행 결과의 레코드 수 반환
+    } 
+    catch(PDOException $Exception)//에러 발생시 $Exception이라는 이름으로 PDO예외 처리 객체 생성
+    {   print "error:".$Exception->getMessage();    }
+
+?>   
+</BODY>
+</HTML>
