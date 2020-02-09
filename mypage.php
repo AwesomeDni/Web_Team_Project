65f67143cc43bcf69f74ee94ff82f83d7e8927a0
hyejin-kim1108 <58838657+hyejin-kim1108@users.noreply.github.com>
2020-02-09 19:05:13 +0900

    2020.02.09 마이페이지(내가 쓴 글만 나오게 수정) -kimhyejin-

--- mypage.php
+++ mypage.php
@@ -5,7 +5,6 @@
 <?php
     require_once("db_conn.php");
     $id=$_SESSION['id'];
-    $email=$_SESSION['email'];
 ?>
 <html>
 <head>
@@ -42,8 +41,7 @@
             <ul>
                 <center>
                     <li><?=$id?>님</li>
-                    <li>$email."님"</li>
-                    <br><br><hr>
+                    <br><br><br><hr>
                     <li><a href="#list">내가 적은글</a></li>
                     <br><br>
                     <li class="out">
@@ -65,46 +63,47 @@
                 <?php
                 # DB연결
                 $pdo = DB_conn();
-                
+
                 // 페이지 설정
                 $page_set = 10; // 한페이지 줄수
                 $block_set = 5; // 한페이지 블럭수
-                
+
                 try
                 {   //쿼리문 작성
-                    $query = "SELECT count(content_no) as total FROM show_view";
+                    $query = "SELECT count(content_no) as total FROM show_view WHERE id='$id'";
                     $stmh=$pdo->prepare($query); //sql문을 인잭션으로 부터 보호하기위한 처리
                     $stmh->execute();
                 }
                 catch(PDOException $e)
                 {   print 'err: '. $e->getMessage();   }
-                
+
                 $row=$stmh->fetch(PDO::FETCH_ASSOC);
-                 
+
                 $total = $row['total']; // 전체글수
-                 
+
                 $total_page = ceil ($total / $page_set); // 총페이지수(올림함수)
                 $total_block = ceil ($total_page / $block_set); // 총블럭수(올림함수)
-                 
+
                 $page = 1; // 현재페이지(넘어온값)
                 if(isset($_GET['page'])){    $page= $_GET['page'];  }
-                
+
                 $block = ceil ($page / $block_set); // 현재블럭(올림함수)
                 $limit_idx = ($page - 1) * $page_set; // 글 시작위치
                 # 게시글 불러오기
                 try
                 {   //쿼리문 작성
-                    $query = "SELECT content_no,title,id,view_cnt from show_view order by content_no desc limit $limit_idx, $page_set";
+                    $query = "SELECT content_no, title, view_cnt FROM show_view WHERE id='$id'" ;
+                    //SELECT content_no,title,id,view_cnt from show_view order by content_no desc limit $limit_idx, $page_set";
                     $stmh=$pdo->prepare($query); //sql문을 인잭션으로 부터 보호하기위한 처리
                     $stmh->execute();
                 }
                 catch(PDOException $e)
                 {   print 'err: '. $e->getMessage();   }
                 ?>
-                
+
                 <table border=1>
                     <tr align="center">
-                        <td>글번호</td><td>제목</td><td>작성자</td><td>조회수</td>
+                        <td>글번호</td><td>제목</td><td>조회수</td>
                     </tr>
                 <?php
                 # 게시글 리스트 출력
@@ -112,47 +111,45 @@
                 {   print "<tr>";
                     print "<td align=center>" . $row['content_no'] . "</td>";
                     print "<td width=500px><a href='show.php?content_no=". $row['content_no'] ."'>" . $row['title'] . "</a></td>";
-                    print "<td align=center>".$row['id']."</td>";
                     print "<td align=center>".$row['view_cnt']."</td>";
                     print "</tr>";
                 }
                 ?>
                 </table>
                 <?php
-                 
+
                 // 페이지번호 & 블럭 설정
                 $first_page = (($block - 1) * $block_set) + 1; // 첫번째 페이지번호
                 $last_page = min ($total_page, $block * $block_set); // 마지막 페이지번호
-                 
+
                 $prev_page = $page - 1; // 이전페이지
                 $next_page = $page + 1; // 다음페이지
-                 
+
                 $prev_block = $block - 1; // 이전블럭
                 $next_block = $block + 1; // 다음블럭
-                 
+
                 // 이전블럭을 블럭의 마지막으로 하려면...
                 $prev_block_page = $prev_block * $block_set; // 이전블럭 페이지번호
                 // 이전블럭을 블럭의 첫페이지로 하려면...
                 //$prev_block_page = $prev_block * $block_set - ($block_set - 1);
                 $next_block_page = $next_block * $block_set - ($block_set - 1); // 다음블럭 페이지번호
-                 
+
                 // 페이징 화면
                 print ($prev_page > 0) ? "<a href='".$_SERVER['PHP_SELF']."?page=$prev_page'>[prev]</a> " : "[prev] ";
                 print ($prev_block > 0) ? "<a href='".$_SERVER['PHP_SELF']."?page=$prev_block_page'>...</a> " : "... ";
-                 
+
                 for ($i=$first_page; $i<=$last_page; $i++) 
                 {   print ($i != $page) ? "<a href='".$_SERVER['PHP_SELF']."?page=$i'>$i</a> " : "<b>$i</b> ";
                 }
-                 
+
                 print ($next_block <= $total_block) ? "<a href='".$_SERVER['PHP_SELF']."?page=$next_block_page'>...</a> " : "... ";
                 print ($next_page <= $total_page) ? "<a href='".$_SERVER['PHP_SELF']."?page=$next_page'>[next]</a>" : "[next]";
                 ?>
-                <button onclick="location.href='insert.php'">글쓰기</button>
                 </body>
                 </html>
-                ?>
+
             </article>
         </center>
     </sction>
 </body>
-</html>
+</html> 
