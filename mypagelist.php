6a1473f444bf65eb4145437624f1b50e59c2181e
hyejin-kim1108 <58838657+hyejin-kim1108@users.noreply.github.com>
2020-02-24 18:03:58 +0900

    2020.02.24 마이페이지리스트( 폰트변경)-kimhyejin-

--- mypagelist.php
+++ mypagelist.php
@@ -3,13 +3,23 @@
 <?php
 $id=$_SESSION['id'];
 ?>
+<style>
+    @font-face{font-family:'A타이틀고딕2'; src:url('A타이틀고딕2.woff');} 
+    .list
+    {font-family:'A타이틀고딕2';}
+    @font-face{font-family:'A펜글씨B'; src:url('A펜글씨B.woff');}
+    .name
+    {font-family:'A펜글씨B';}
+</style>
 </head>
 <body>
     <!--내가 쓴글-->
     <div class="list table table-bordered">
         <center>
-            <br>
-            <h3>내가 쓴 글</h3>
+            <div class="name">
+                <br>
+                <h3>내가 쓴 글</h3>
+            </div>
             <br><br>
             <article>
                 <?php
@@ -52,58 +62,57 @@ $id=$_SESSION['id'];
                 catch(PDOException $e)
                 {   print 'err: '. $e->getMessage();   }
                 ?>
-
-                <table border=1>
-                    <tr align="center">
-                        <td>글번호</td><td>제목</td><td>조회수</td>
-                    </tr>
-                <?php
-                # 게시글 리스트 출력
-                while($row=$stmh->fetch(PDO::FETCH_ASSOC))
-                {   print "<tr>";
-                    print "<td align=center>" . $row['content_no'] . "</td>";
-                    print "<td width=500px><a href='show.php?content_no=". $row['content_no'] ."'>" . $row['title'] . "</a></td>";
-                    print "<td align=center>".$row['view_cnt']."</td>";
-                    print "</tr>";
-                }
-                ?>
-                </table>
-                <!-- 페이징 -->
-            <span class="paging">
-                <?php
-                // 페이지번호 & 블럭 설정
-                $first_page = (($block - 1) * $block_set) + 1; // 첫번째 페이지번호
-                $last_page = min ($total_page, $block * $block_set); // 마지막 페이지번호
-                $prev_page = $page - 1; // 이전페이지
-                $next_page = $page + 1; // 다음페이지
-                $prev_block = $block - 1; // 이전블럭
-                $next_block = $block + 1; // 다음블럭
-                // 이전블럭을 블럭의 마지막으로 하려면...
-                $prev_block_page = $prev_block * $block_set; // 이전블럭 페이지번호
-                // 이전블럭을 블럭의 첫페이지로 하려면...
-                //$prev_block_page = $prev_block * $block_set - ($block_set - 1);
-                $next_block_page = $next_block * $block_set - ($block_set - 1); // 다음블럭 페이지번호
-                // 페이징 화면
-                if(isset($key)) // 검색 키워드가 있을시
-                {   print ($prev_block > 0) ? "<a href='".$_SERVER['PHP_SELF']."?search=$key&category=$category&page=$prev_block_page'>[이전]</a> " : "[이전] ";   }
-                else // 아니면
-                {   print ($prev_block > 0) ? "<a href='".$_SERVER['PHP_SELF']."?category=$category&page=$prev_block_page'>[이전]</a> " : "[이전] ";   }
-
-                for ($i=$first_page; $i<=$last_page; $i++) 
-                {   if(isset($key)) // 검색 키워드가 있을시
-                    {   print ($i != $page) ? "<a href='".$_SERVER['PHP_SELF']."?search=$key&category=$category&page=$i'>$i</a> " : "<b>$i</b> ";   }
+                <div class="list">
+                    <table border=1>
+                        <tr align="center">
+                            <td>글번호</td><td>제목</td><td>조회수</td>
+                        </tr>
+                    <?php
+                    # 게시글 리스트 출력
+                    while($row=$stmh->fetch(PDO::FETCH_ASSOC))
+                    {   print "<tr>";
+                        print "<td align=center>" . $row['content_no'] . "</td>";
+                        print "<td width=500px><a href='show.php?content_no=". $row['content_no'] ."'>" . $row['title'] . "</a></td>";
+                        print "<td align=center>".$row['view_cnt']."</td>";
+                        print "</tr>";
+                    }
+                    ?>
+                    </table>
+                    <!-- 페이징 -->
+                <span class="paging">
+                    <?php
+                    // 페이지번호 & 블럭 설정
+                    $first_page = (($block - 1) * $block_set) + 1; // 첫번째 페이지번호
+                    $last_page = min ($total_page, $block * $block_set); // 마지막 페이지번호
+                    $prev_page = $page - 1; // 이전페이지
+                    $next_page = $page + 1; // 다음페이지
+                    $prev_block = $block - 1; // 이전블럭
+                    $next_block = $block + 1; // 다음블럭
+                    // 이전블럭을 블럭의 마지막으로 하려면...
+                    $prev_block_page = $prev_block * $block_set; // 이전블럭 페이지번호
+                    // 이전블럭을 블럭의 첫페이지로 하려면...
+                    //$prev_block_page = $prev_block * $block_set - ($block_set - 1);
+                    $next_block_page = $next_block * $block_set - ($block_set - 1); // 다음블럭 페이지번호
+                    // 페이징 화면
+                    if(isset($key)) // 검색 키워드가 있을시
+                    {   print ($prev_block > 0) ? "<a href='".$_SERVER['PHP_SELF']."?search=$key&category=$category&page=$prev_block_page'>[이전]</a> " : "[이전] ";   }
                     else // 아니면
-                    {   print ($i != $page) ? "<a href='".$_SERVER['PHP_SELF']."?category=$category&page=$i'>$i</a> " : "<b>$i</b> ";   }
-                    
-                }
-                if(isset($key)) // 검색 키워드가 있을시
-                {   print ($next_page <= $total_page) ? "<a href='".$_SERVER['PHP_SELF']."?search=$key&category=$category&page=$next_block_page'>[다음]</a>" : "[다음]";    }
-                else // 아니면
-                {   print ($next_page <= $total_page) ? "<a href='".$_SERVER['PHP_SELF']."?category=$category&page=$next_block_page'>[다음]</a>" : "[다음]";    }
+                    {   print ($prev_block > 0) ? "<a href='".$_SERVER['PHP_SELF']."?category=$category&page=$prev_block_page'>[이전]</a> " : "[이전] ";   }
 
-                ?>
-            </span>
-            <button onclick="location.href='insert.php'" class="info">글쓰기</button>
+                    for ($i=$first_page; $i<=$last_page; $i++) 
+                    {   if(isset($key)) // 검색 키워드가 있을시
+                        {   print ($i != $page) ? "<a href='".$_SERVER['PHP_SELF']."?search=$key&category=$category&page=$i'>$i</a> " : "<b>$i</b> ";   }
+                        else // 아니면
+                        {   print ($i != $page) ? "<a href='".$_SERVER['PHP_SELF']."?category=$category&page=$i'>$i</a> " : "<b>$i</b> ";   }
+                        
+                    }
+                    if(isset($key)) // 검색 키워드가 있을시
+                    {   print ($next_page <= $total_page) ? "<a href='".$_SERVER['PHP_SELF']."?search=$key&category=$category&page=$next_block_page'>[다음]</a>" : "[다음]";    }
+                    else // 아니면
+                    {   print ($next_page <= $total_page) ? "<a href='".$_SERVER['PHP_SELF']."?category=$category&page=$next_block_page'>[다음]</a>" : "[다음]";    }
+                    ?>
+                </span>
+                </div>
     </div>
 </body>
 </html>
